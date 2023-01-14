0325b29a0349878eb8f52d3a0179bb0d1f5246e0
MegaPanter <113056259+MegaPanter@users.noreply.github.com>
2023-01-13 20:05:06 -0600

    holi

---
+++ Practica_3TM4C129/lib/UART.h
@@ -0,0 +1,13 @@
+#ifndef UART_UART_H_
+#define UART_UART_H_
+#include <string.h>
+
+extern void Configurar_UART1(void);
+extern char readChar(void);
+extern void printChar(char c);
+extern void printString(char* string);
+extern int readString(char delimitador, char *string,char*string2);
+extern char invertirString(char *string);
+
+
+#endif /* UART_UART_H_ */
